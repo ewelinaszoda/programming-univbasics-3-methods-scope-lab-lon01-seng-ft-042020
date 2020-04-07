7eb1eb8a14997b4b39ca9cdefb9d4f4c2e2e2d73
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-07 13:39:56 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,8 +10,8 @@ def toadstool
 end
 
 def link
-  $phrase = "It's Dangerous To Go Alone! Take This."
-  puts $phrase
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
 end 
 
 def all_phrases
