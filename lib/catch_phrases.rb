739990fb15a3a106aea2d61ab67c8638cbc7317d
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-07 13:36:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  $phrase = "It's-a me, Mario!"
+  puts $phrase
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link
+  sentense = "It's Dangerous To Go Alone! Take This."
+  puts sentense 
+end 
+
+def all_phrases
+  puts $phrase
+end 
